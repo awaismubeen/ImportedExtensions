273261e08ee4839208098a3e9f5b771e0a8a56b0
Awais Mubeen <awaismubeen@gmail.com>
2025-07-11 12:41:45 +0500

    Rounded fixed

--- Sources/ImportedExtensions/IndispensableExtensions.swift
+++ Sources/ImportedExtensions/IndispensableExtensions.swift
@@ -1534,12 +1534,12 @@ extension UITableViewController {
     }
 }
 extension Double {
-    /// Rounds the double to decimal places value
-    func rounded(toPlaces places:Int) -> Double {
-        let divisor = Double.pow(10.0, Double(places))
+    func rounded(toPlaces places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
     }
 }
+
 extension UIButton {
     func setTintColor(color: UIColor) {
         let templateImage = self.imageView?.image?.withRenderingMode(.alwaysTemplate)
