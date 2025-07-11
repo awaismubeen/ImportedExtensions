169c22e15f4fb518aeb338ef9700e13e55fd4d21
Awais Mubeen <awaismubeen@gmail.com>
2025-07-11 11:20:57 +0500

    SPM
    creating SPM

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "ImportedExtensions",
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v10_13)
+    ],
+    products: [
+        .library(
+            name: "ImportedExtensions",
+            targets: ["ImportedExtensions"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "ImportedExtensions",
+            path: "Sources/ImportedExtensions"
+        )
+    ]
+)
+
